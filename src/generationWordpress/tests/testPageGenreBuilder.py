efe93b976c08a351c2233b2aac4890c3f22b228f
moissina <jcmoissinac@gmail.com>
2024-11-02 17:23:23 +0100

    ébauches de test unitaire, à approfondir

---
+++ src/generationWordpress/tests/testPageGenreBuilder.py
@@ -0,0 +1,13 @@
+import unittest
+import src.generationWordpress.PageBuilder as PageBuilder
+class TestGenreBuilder(unittest.TestCase):
+    def test_genre_portrait(self):
+        qid = "Q134307"
+        # create PageBuilder
+        pbGenreFr = PageBuilder.PageBuilder("Q1792379") # genre, default lang fr
+        page = pbGenreFr.build_scrutart_page("Q134307") # portrait
+        pass
+        self.assertTrue("""{ "qid": "Q134307", "templateVersion": "1.0.2" }""" in page)
+
+if __name__=='__main__':
+    unittest.main()
