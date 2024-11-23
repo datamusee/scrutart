d14704236506e4f9e60d8706cb3340579759fbcb
moissina <jcmoissinac@gmail.com>
2024-11-23 23:35:28 +0100

    modif mineures

--- src/piwigoTools/envoiImagePiwigo.py
+++ src/piwigoTools/envoiImagePiwigo.py
@@ -4,8 +4,10 @@ import datetime
 import json
 import time
 
-def postImageToPiwigo(im):
-    image_path = im["image"]
+def postImageToPiwigo(im, piwigoCategory=15, categoryName="portrait"):
+    # dans la version 2, il peut y avoir plusieurs images, je prend la première
+    # il faudrait prendre la meilleure, de préférence sans le cadre
+    image_path = im["images"][0]
     hh = { 'User-Agent': 'Scrutart-UA (https://scrutart.grains-de-culture.fr/; scrutart@grains-de-culture.fr)'}
     image_response = requests.get(image_path, headers=hh)
     image_data = image_response.content
@@ -31,11 +33,11 @@ def postImageToPiwigo(im):
         qid = uri.replace("http://www.wikidata.org/entity/", "")
         crea = im["createur"]
         creaLab = im["createurLabel"]
-        categorie_id = 15  # ID de la catégorie dans laquelle ajouter l'image
+        categorie_id = piwigoCategory  # ID de la catégorie dans laquelle ajouter l'image
         # todo ajouter nom créateur dans description
         # todo gérer description multilingue
         description = f"""Créateur: <a href="{crea}">{creaLab}</a><div>Source Wikimedia Commons <a href='{image_path}'>{image_path}</a></div><div>Elément Wikidata <a href='{uri}'>{qid}</a></div>"""
-        tags = ["portrait"]  # Liste des tags à associer à l'image
+        tags = [categoryName]  # Liste des tags à associer à l'image
 
         # Authentification et envoi de l'image avec des métadonnées
         payload = {
@@ -58,29 +60,76 @@ def postImageToPiwigo(im):
         if response.status_code == 200:
             print(f"L'image '{titre}' a été téléchargée avec succès et les métadonnées ont été ajoutées !")
             print(datetime.datetime.now())
-            return True
+            return response
         else:
             print("Erreur :", response.status_code, response.text)
-            return False
+            return None
+def convertFromV1To(data, versionTarget):
+    targetData = data
+    if "version" in data and data["version"]!="1.0.0":
+        return data
+    if versionTarget=="2.0.0":
+        listim = data["liste"]
+        targetData["dict"] = {}
+        targetData["version"] = "2.0.0"
+        for im in listim:
+            imuri = im["uri"]
+            if not imuri in targetData["dict"]:
+                targetData["dict"][imuri] = {}
+                targetData["dict"][imuri]["uri"] = imuri
+                if "createur" in im: targetData["dict"][imuri]["createur"] = im["createur"]
+                if "createurLabel" in im: targetData["dict"][imuri]["createurLabel"] = im["createurLabel"]
+                if "titre_fr" in im: targetData["dict"][imuri]["titre_fr"] = im["titre_fr"]
+                if "posted" in im: targetData["dict"][imuri]["posted"] = im["posted"]
+                if "image" in im: targetData["dict"][imuri]["images"] = [im["image"]]
+                pass
+            else:
+                if "image" in im: targetData["dict"][imuri]["images"].append(im["image"])
+        if "liste" in targetData: targetData["liste"] = []
+    return targetData
+
+def simpleCopy(data, versionTarget):
+    return data
+
+def convertListImage(data, versionSrc, versionTarget):
+    knowVersions = {
+        "1.0.0": { "desc" : "version initiale composée d'une liste obtenue par téléchargement sur WDQS auquel on a ajouté un champ sparql pour garder trace de la requ^te qui a servi à générer ces données"},
+        "2.0.0": { "desc": "évolution de version 1, pour éviter d'envoyer plusieurs images pour une même oeuvre; la liste est remplacée par un dictionnaire; c'est l'oeuvre qui est marquée comme postée, pas l'image" }
+    }
+    dataConverter = {
+        "1.0.0": convertFromV1To
+    }
+    targetData = dataConverter.get(versionSrc, simpleCopy)(data,versionTarget)
+    return targetData
 
 if __name__=="__main__":
-    listimagespath = "D:/wamp64/www/givingsense.eu/datamusee/scrutart/src/generationWordpress/data/fr/listeImagesPortrait.json"
-    listim = []
+    categoryName = "Pissarro"
+    piwigoCategory = 40
+    listimagespath = "D:/wamp64/www/givingsense.eu/datamusee/scrutart/src/generationWordpress/data/fr/listeImagesCamillePissarro.json"
+    dictim = {}
     with open(listimagespath, "r", encoding="UTF-8") as fdata:
         data = json.loads(fdata.read())
-        listim = data["liste"]
-    if listim:
+        if not "version" in data:
+            data = convertListImage(data, "1.0.0", "2.0.0")
+        dictim = data["dict"]
+    if dictim:
         print(datetime.datetime.now())
-        for idx, im in enumerate(listim[77:1007]):
+        freqsav = 5
+        idxsav = 0
+        for uri, im in dictim.items():
             if not "posted" in im:
-                res = postImageToPiwigo(im)
+                res = postImageToPiwigo(im, piwigoCategory, categoryName)
                 if res:
+                    im["post_result"] = res.text
                     im["posted"] = True
-                    listim[idx] = im
-                time.sleep(300)
-            with open(listimagespath, "w", encoding="UTF-8") as fdata:
-                data["liste"] = listim
-                json.dump(data, fdata, ensure_ascii=False)
+                    dictim[uri] = im
+                    idxsav += 1
+                    time.sleep(50)
+                    if idxsav>=freqsav:
+                        idxsav = 0
+                        with open(listimagespath, "w", encoding="UTF-8") as fdata:
+                            data["dict"] = dictim
+                            json.dump(data, fdata, ensure_ascii=False)
 
 
 # La description peut contenir un lien vers l entité source, un lien vers l URL de l image, le copyright
