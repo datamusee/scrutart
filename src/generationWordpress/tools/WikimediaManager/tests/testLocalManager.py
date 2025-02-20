150cfdd29d98af564db89d0daacbd796e1a982cd
moissina <jcmoissinac@gmail.com>
2025-02-20 17:27:55 +0100

    test de l'api manager local

--- src/generationWordpress/tools/WikimediaManager/tests/testLocalManager.py
+++ src/generationWordpress/tools/WikimediaManager/tests/testLocalManager.py
@@ -3,9 +3,10 @@ import json
 import time
 import requests
 from ..WikimediaManagerPackage.configPrivee import config
+from urllib.parse import urlencode, urlunsplit
 
-#baseurl = "http://127.0.0.1:5000"
-baseurl = "https://webapimanager.grains-de-culture.fr"
+baseurl = "http://127.0.0.1:6000"
+#baseurl = "https://webapimanager.grains-de-culture.fr"
 
 
 def createManager(targetUrls, bearer=None):
@@ -72,6 +73,46 @@ class MyTestCase(unittest.TestCase):
         deleteManager(manager_id, bearer=bearer)
         self.assertEqual(ok, True, "Manager créé, requête envoyée, manager détruit")
 
+
+    def test_un_appel_synchrone_wikidata(self):
+        bearer = config['admin']["Bearer"]
+        headers = {f"Authorization": f"Bearer {bearer}"} if bearer else {}
+        query = "https://query.wikidata.org/sparql"
+        manager_id = createManager([query], bearer=bearer)
+        # limit est le nombre d'appels par seconde pour ce manager
+        data = { "manager_id": manager_id, "limit": 1 }
+        lim = requests.post(f"{baseurl}/api/set_rate_limit", json=data, headers=headers)
+        cachedur = 60  # 0;  en secondes
+        parameters = {"query": "select * where { ?s ?p ?o } limit 10", "format": "JSON"}
+        encodedquery = urlencode(query=parameters, doseq=True)
+        data = {
+            "manager_id": manager_id,
+            "url": urlunsplit(("https", "query.wikidata.org", "/sparql", encodedquery, "")),
+            "method": "GET",
+            "cache_duration": cachedur,
+            "headers": {
+                "User-Agent": "Scrutart-UA (https://scrutart.grains-de-culture.fr/; scrutart@grains-de-culture.fr)"
+            }
+        }
+        # mise de la requête dans la file d'attente
+        req = requests.get(f"{baseurl}/api/request", json=data, headers=headers)
+        uuidreq = json.loads(req.text)["uuid"]
+        statusUrl = f"{baseurl}{json.loads(req.text)['status_url']}"
+        while True:
+            stat = requests.get(statusUrl, headers=headers)
+            jstat = json.loads(stat.text)
+            if "status" in jstat and jstat["status"] == "complete":
+                rep = jstat["response"]
+                break
+            elif "status" in jstat and jstat["status"] == "pending":
+                time.sleep(0.1)
+            else:
+                # print(jstat)
+                pass
+        print(rep)
+        ok = (stat.status_code == 200) and ("response" in jstat) and ("results" in jstat["response"])
+        deleteManager(manager_id, bearer=bearer)
+        self.assertEqual(ok, True, "Manager créé, requête envoyée, manager détruit")
 
 if __name__ == '__main__':
     # lancer le manager
