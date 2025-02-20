772d5fac015ad2c113540c47a4529e0133d6d576
moissina <jcmoissinac@gmail.com>
2025-02-20 17:11:59 +0100

    liste d'entités wikidata auxquelles on veut s'intéresser dans différents traitements; à ce jour utilisé par generationCreateursFrAnalysesWikidata.py et buildAlbumsTypedList.py

---
+++ src/generationWordpress/entitiesList.py
@@ -0,0 +1,41 @@
+entitiesList = [
+    "Q5597", # Raphaël
+    "Q34618", # courbet
+    "Q45205", # Dufy
+    "Q187310", # Carl Larsson
+    "Q206820", # Anders Zorn
+    "Q297838", # José de Ribera
+    "Q730008", # Bruno Liljefors
+    "Q37693", # Gauguin
+    "Q295144", # Caillebotte
+    "Q61064", # Kandinsky
+ "Q239394", #"title": "Où trouver Édouard Vuillard dans Wikidata, suivez le guide", "idwordpress": "892", "status": "publish", "date": "2024-12-10T10:09:10" }
+ "Q435316", #"title": "Où trouver Giandomenico Tiepolo dans Wikidata, suivez le guide", "idwordpress": "898", "status": "publish", "date": "2024-12-10T10:02:26" }
+ "Q40599", #"title": "Où trouver Édouard Manet dans Wikidata, suivez le guide", "idwordpress": "897", "status": "publish", "date": "2024-11-23T11:51:12" }
+ "Q46373", #"title": "Où trouver Edgar Degas dans Wikidata, suivez le guide", "idwordpress": "899", "status": "publish", "date": "2024-11-23T11:45:48" }
+ "Q187506", #"title": "Où trouver Honoré Daumier dans Wikidata, suivez le guide", "idwordpress": "916", "status": "publish", "date": "2024-11-23T11:39:48" }
+ "Q241732", #"title": "Où trouver Rosa Bonheur dans Wikidata, suivez le guide", "idwordpress": "151", "status": "publish", "date": "2024-11-23T11:03:14" }
+ "Q175130", #"title": "Où trouver Alfred Sisley dans Wikidata, suivez le guide", "idwordpress": "110", "status": "publish", "date": "2024-12-31T09:35:58" }
+ "Q34618", #"title": "Où trouver Gustave Courbet dans Wikidata, suivez le guide", "idwordpress": "186", "status": "publish", "date": "2024-11-23T10:50:44" }
+ "Q134741", #"title": "Où trouver Camille Pissarro dans Wikidata, suivez le guide", "idwordpress": "63", "status": "publish", "date": "2024-11-23T10:44:54" }
+ "Q180932", #"title": "Où trouver François Boucher dans Wikidata, suivez le guide", "idwordpress": "111", "status": "publish", "date": "2024-11-25T12:18:27" }
+ "Q5577", #"title": "Où trouver Salvador Dalí dans Wikidata, suivez le guide", "idwordpress": "902", "status": "publish", "date": "2024-10-17T12:42:53" }
+ "Q5593", #"title": "Où trouver Pablo Picasso dans Wikidata, suivez le guide", "idwordpress": "928", "status": "publish", "date": "2024-11-01T11:31:04" }
+ "Q5580", #"title": "Où trouver Albrecht Dürer dans Wikidata, suivez le guide", "idwordpress": "903", "status": "publish", "date": "2024-10-18T06:01:56" }
+ "Q153920", #"title": "Où trouver Aristide Maillol dans Wikidata, suivez le guide", "idwordpress": "889", "status": "publish", "date": "2024-10-17T12:39:16" }
+ "Q151679", #"title": "Où trouver Roy Lichtenstein dans Wikidata, suivez le guide", "idwordpress": "887", "status": "publish", "date": "2024-10-17T12:38:29" }
+ "Q109061", #"title": "Où trouver Guido Reni dans Wikidata, suivez le guide", "idwordpress": "847", "status": "publish", "date": "2024-10-16T21:22:38" }
+ "Q209050", #"title": "Où trouver Jan Brueghel l&#8217;Ancien dans Wikidata, suivez le guide", "idwordpress": "135", "status": "publish", "date": "2024-01-30T20:53:05" }
+ "Q29231", #"title": "Où trouver Frans Snyders dans Wikidata, suivez le guide", "idwordpress": "162", "status": "publish", "date": "2024-01-30T20:52:13" }
+ "Q296", #"title": "Où trouver Claude Monet dans Wikidata, suivez le guide", "idwordpress": "163", "status": "publish", "date": "2024-10-14T13:41:01" }
+ "Q301", #"title": "Où trouver Le Greco dans Wikidata, suivez le guide", "idwordpress": "165", "status": "publish", "date": "2024-01-30T20:52:12" }
+ "Q45205", #"title": "Où trouver Raoul Dufy dans Wikidata, suivez le guide", "idwordpress": "211", "status": "publish", "date": "2024-11-23T10:26:08" }
+ "Q5598", #"title": "Où trouver Rembrandt dans Wikidata, suivez le guide", "idwordpress": "239", "status": "publish", "date": "2024-01-30T20:50:27" }
+ "Q5599", #"title": "Où trouver Pierre Paul Rubens dans Wikidata, suivez le guide", "idwordpress": "240", "status": "publish", "date": "2024-11-23T06:18:59" }
+ "Q708629", #"title": "Où trouver William Daniell dans Wikidata, suivez le guide", "idwordpress": "256", "status": "publish", "date": "2024-01-30T20:49:53" }
+ "Q1470", #"title": "Où trouver Maurits Cornelis Escher dans Wikidata, suivez le guide", "idwordpress": "68", "status": "publish", "date": "2024-01-30T20:28:21" }
+ "Q150679", #"title": "Où trouver Antoine van Dyck dans Wikidata, suivez le guide", "idwordpress": "70", "status": "publish", "date": "2024-01-30T20:28:20" }
+ "Q920924", #"title": "Où trouver Leslie Ward dans Wikidata, suivez le guide", "idwordpress": "272", "status": "publish", "date": "2024-01-30T20:22:53" }
+ "IDWP_277", #"title": "Où trouver Paul Véronèse dans Wikidata, suivez le guide", "idwordpress": "277", "status": "publish", "date": "2024-01-30T20:22:52" }
+ "Q183221", #"title": "Où trouver Watteau dans Wikidata, suivez le guide", "idwordpress": "8", "status": "publish", "date": "2024-01-31T05:50:45" }
+]
