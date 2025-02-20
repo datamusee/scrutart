5bff7c9aac6b23a3bc96bb7bc4625ac872fb6c33
moissina <jcmoissinac@gmail.com>
2025-02-20 17:06:56 +0100

    ébauche de commentaire pour faire une analyse des titres d'oeuvres et en tirer des annotations: lieux, objets ou personnes représentés

---
+++ src/analyses/analysesTitres.py
@@ -0,0 +1,11 @@
+"""
+but: obtenir des indications validées d'éléments du sujet d'une œuvre
+principalement à partir des titres
+corroborés avec d'autres éléments, comme les pages wikipedia associées à l'œuvre
+cela peut être la présence d'une personne ou d'un lieu, le genre -comme nu ou portrait-...
+"""
+"""
+exemples
+titre Nude girl de Anders Zorn -> genre 'nu'
+titre Le Pont Boieldieu, Rouen, Soleil Couchant de Camille Pissarro -> Pont Boeildieu et Rouen
+"""
