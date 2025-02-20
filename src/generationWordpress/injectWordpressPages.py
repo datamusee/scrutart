6b2d2960f1b34f89e9132e86e2231e0706595d97
moissina <jcmoissinac@gmail.com>
2025-02-20 17:46:43 +0100

    exemple élémentaire d'injection d'une page dans le wordpress scrutart

--- src/generationWordpress/injectWordpressPages.py
+++ src/generationWordpress/injectWordpressPages.py
@@ -1,12 +1,12 @@
 import requests
-import configPrivee
+import configPrivee2 as configPrivee
 
 #api_url = f"{configPrivee.WORDPRESS_API_URL}/posts?Authorization=Bearer{configPrivee.WORDPRESS_PASSWORD_APP}"
 #auth = (configPrivee.WORDPRESS_USERNAME, configPrivee.WORDPRESS_PASSWORD_APP)
 api_url = f"{configPrivee.WORDPRESS_O2_API_URL}/posts?Authorization=Bearer{configPrivee.WORDPRESS_O2_PASSWORD_APP}"
-auth = (configPrivee.WORDPRESS_O2_USERNAME, configPrivee.WORDPRESS_O2_PASSWORD_APP)
+auth = (configPrivee.WORDPRESS_O2_API2USERNAME, configPrivee.WORDPRESS_O2_API2PASSWORD_APP)
 data = {
-    "title": "Article de test d'automation 3",
+    "title": "Article de test d'automation 4",
     "content": "Article 3 injecté avec injectWordpressPages.py",
     "status": "draft",
     "slug": "article-test-automation",
