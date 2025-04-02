4ce09ad1b6c7934258fc4f116f9ad69d3c70f3ff
moissina <jcmoissinac@gmail.com>
2025-04-02 15:35:17 +0200

    ajout de commentaires

--- src/generationWordpress/dumpListToDict.py
+++ src/generationWordpress/dumpListToDict.py
@@ -1,11 +1,17 @@
 import json
 import re
 
+# but: prendre un dump des post sous forme de liste, reconnaitre les QIDs concernés
+# s'en servir de clé pour construire un dict avec le dump des posts liés à un QID
+
+# fichier qui contient un dump des posts présents dans le wordpress scrutart
 dumpfile = "dumps/dumpAPIScrutartPosts20250202.json"
 with open(dumpfile, "r", encoding="UTF-8") as fdump:
     posts = json.load(fdump)
 
+# regex pour voir si le champ raw d'un article contient un json de metadata d'un artiste avec son QID
 regex1 = r".*\n.*\"qid\": \"(Q[0-9]*)"
+# regex qui permet de tester si la phrase d'introduction d'une analyse liée à un QID est présente
 regex2 = r"p>Je vais faire dans ce billet une analyse de la présence des œuvres de <a href=\"http://www.wikidata.org/entity/(Q[0-9]*)"
 
 posts_dict = {}
