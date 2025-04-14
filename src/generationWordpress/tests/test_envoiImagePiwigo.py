f55d9b6a34ec40f9cd28ff6211eb99f9298fe282
moissina <jcmoissinac@gmail.com>
2025-04-14 17:37:13 +0200

    premier commit de ce test élémentaire d'envoi d'une image à Piwigo

---
+++ src/generationWordpress/tests/test_envoiImagePiwigo.py
@@ -0,0 +1,24 @@
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath('D:/wamp64/www/givingsense.eu/src/piwigoTools'))
+
+from unittest import TestCase
+from src.piwigoTools.envoiImagePiwigo import postImageToPiwigo
+import src.piwigoTools.configPiwigo as cp
+
+
+class Test(TestCase):
+    def test_post_image_to_piwigo(self):
+        im = {
+      "uri": "http://www.wikidata.org/entity/Q15874516",
+      "createur": "http://www.wikidata.org/entity/Q45006",
+      "createurLabel": "Tests",
+      "titre_fr": "Chat2",
+      "images": [
+        "https://jardinage.lemonde.fr/images/dossiers/2017-10/chatte-155436.jpg"
+      ]
+        }
+        res = postImageToPiwigo(im, piwigoCategory=852, categoryName="Tests", cp=cp)
+        self.assertEqual(res.text, """{"stat":"ok","result":{"image_id":"18439","url":"https:\/\/galleries.grains-de-culture.fr\/picture.php?\/18439\/category\/852"}}""")
+        pass
