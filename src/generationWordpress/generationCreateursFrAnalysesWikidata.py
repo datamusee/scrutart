a03d1d9edba0c92c70338b806db817639c0ee802
moissina <jcmoissinac@gmail.com>
2025-04-14 16:44:44 +0200

    ajout d'un main ouverture initiale de l'accès à scrutart-state

--- src/generationWordpress/generationCreateursFrAnalysesWikidata.py
+++ src/generationWordpress/generationCreateursFrAnalysesWikidata.py
@@ -1,73 +1,40 @@
 # but: générer des contenus prêts à être intégrés dans un wordpress
-# part d'un template basé sur le billet
-# https://datamusee.wp.imt.fr/fr/2024/01/12/paul-gauguin-dans-wikidata/
-# récupération de la source de cette page comme base de template
-# ce code source est copiée de generationWordpress\generationCreateursFrAnalysesWikidata.py
-# il parait souhaitable d'homogénéiser le code pour les différentes langues et les différents thèmes
-# dans le cas des créateurs, on a relativement peu d'instances d'œuvres et pas rencontré de timeout
-import datetime
-import time
+# voir ticket 1527 pour un peu mieux identifier les créateurs candidats
 import json
 import os
-import src.generationWordpress.PageBuilder as PageBuilder
-import SPARQLWrapper as sw
+from PageBuilder import PageBuilder
 from entitiesList import entitiesList
+from tools.scrutartStateManager import scrutartStateManager
 
-filterEntities = entitiesList
-def checkIfSparqlScrutartStateEndpointIsAvailable(urlendpoint):
-    query = """select distinct ?s            where {              ?s ?p ?o            } LIMIT 1        """
-    sparqlScrutartWrapper = sw.SPARQLWrapper2(urlendpoint)  # implicit JSON format
-    sparqlScrutartWrapper.setQuery(query)
-    try:
-        res = sparqlScrutartWrapper.queryAndConvert()
-        return True if res and res.bindings else False
-    except Exception as e:
-        return False
-
-# ne traiter que ceux qui sont dans filterEntities en supprimant la page si elle existe déjà
-sparqlScrutartStateEndpoint = "http://127.0.0.1:3030/scrutartState/query"
-if not checkIfSparqlScrutartStateEndpointIsAvailable(sparqlScrutartStateEndpoint):
-    print(
-        f"le serveur scrutart state (au 12/2/2025, {sparqlScrutartStateEndpoint} (D:\Outils\Semantic/apache-jena-fuseki-4.8.0/fuseki-server) doit avoir été lancé avant de lancer cette application")
-    exit(7777)
-filelist = os.listdir("./pages/creator/fr")
-for qid in filterEntities:
-    if "{qid}.wp".format(qid=qid) in filelist:
-        os.remove("pages/creator/fr/{qid}.wp".format(qid=qid))
+def cleanTargetDirectory(targetDir, entityList):
+    if not os.path.exists(targetDir):
+        os.makedirs(targetDir)
+    filelist = os.listdir(targetDir)
+    for qid in entityList:
+        if f"""{qid}.wp""" in filelist:
+            os.remove(f"{targetDir}/{qid}.wp")
 
 
-pageBuilderTypeFr = PageBuilder.PageBuilder("Q1028181")  # type=peintre, default lang fr
-with open("data/wikidataSignificantPaintersTicket1527.json", encoding="UTF-8") as fListTypes:
-    types = json.load(fListTypes)
+if __name__=="__main__":
+    scrutartState = scrutartStateManager("http://127.0.0.1:3030/scrutartState/query")
+    # ne traiter que les entités qui sont dans filterEntities en supprimant la page si elle existe déjà
+    filterEntities =  [   "Q5597" ] # Raphaël entitiesList
+    pageType = "Q1028181"
+    labelType = {
+        "Q1028181": "painter",
+    }
+    pageLang = "fr"
+    targetDir = f"""./pages/{pageType}_{labelType[pageType]}/{pageLang}"""
+    # detruire les pages à générer si elles existent déjà
+    cleanTargetDirectory(targetDir, filterEntities)
+
+    crtPageBuilder = PageBuilder(pageType, lang=pageLang)  # type=peintre, default lang fr
+    types = []
+    with open("data/wikidataSignificantPaintersTicket1527.json", encoding="UTF-8") as fListTypes:
+        types = json.load(fListTypes)
+        filterEntities = [ elmt["entity"].replace("http://www.wikidata.org/entity/", "") for elmt in types ]# ligne à enlever si je ne veux pas tout traiter et tenir compte du filtre
     for p in types:
-        print(p["entity"], " ", p["entityLabel"])
-        try:
-            qid = p["entity"].replace("http://www.wikidata.org/entity/", "")
-            if not qid in filterEntities:
-                continue
-            filename = "pages/creator/fr/{qid}.wp".format(qid=str(qid))
-            with open(filename, "x", encoding="utf-8") as fpage:
-                page = pageBuilderTypeFr.build_scrutart_page(qid)
-                page = pageBuilderTypeFr.nettoyageContenu(page)
-                fpage.write(page)
-                time.sleep(3)
-        except Exception as e: # file already exist or other error
-            print(e)
-            print("possibly a file already exists for ", qid)
-        pass
-""" voir ticket 1527 pour un peu mieux identifier les candidats
-requête pour identifier des artistes candidats
-
-select ?s (count(?oeuvre) as ?c) 
-where 
-{
-  ?s wdt:P106 wd:Q1028181 .
-  ?oeuvre wdt:P170 ?s
-}
-group by ?s
-order by desc(?c)
-#having(?c>500)
-"""
-"""
-faire des billets sur la présence des mouvements picturaux dans Wikidata
-"""
+        qid = p["entity"].replace("http://www.wikidata.org/entity/", "")
+        if not qid in filterEntities:
+            continue
+        crtPageBuilder.generatePage(qid, targetDir)
