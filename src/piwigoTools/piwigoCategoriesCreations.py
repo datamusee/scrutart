10bab1fc8b21fb4e40227b8794ce072d4eb3dea0
moissina <jcmoissinac@gmail.com>
2025-04-14 17:39:09 +0200

    renommage pour rapprocher de d'autres codes de travail sur les catégories Piwigo et compléments de code pour nommer les catégories en fonction de leur type (genres, créateurs, mouvements, institutions)

--- src/piwigoTools/piwigoCategoriesCreations.py
+++ src/piwigoTools/piwigoCategoriesCreations.py
@@ -4,7 +4,7 @@ import datetime
 import json
 import time
 
-def createCategoryInPiwigo(categoryName="portrait"):
+def createCategoryInPiwigo(categoryName="portrait", categoryType="GENRES"):
     username = cp.configPiwigo["login"]
     password = cp.configPiwigo["pass"]
     auth_data = {
@@ -20,12 +20,26 @@ def createCategoryInPiwigo(categoryName="portrait"):
     piwigo_base_url = "https://galleries.grains-de-culture.fr/ws.php"
     response = session.post(piwigo_base_url, data=auth_data)
     if response.ok:  # and response.json().get("stat") == "ok":
+        galeryNaming = {
+            "GENRES": f"Galerie de {categoryName}s",
+            "CREATORS": f"Galerie {categoryName}",
+            "MOVEMENTS": f"{categoryName}",
+            "INSTITUTIONS":f" {categoryName}"
+        }
+        galeryParent = {
+            "GENRES": 81,
+            "CREATORS": 80,
+            "MOVEMENTS": 853,
+            "INSTITUTIONS": 854
+        }
         # print("Connexion réussie!")
         # todo gérer description multilingue
         # Authentification et envoi de l'image avec des métadonnées
+        #nameFormat = "GENRES"
         payload = {
             "status": "private",
-            "name": f"Galerie de {categoryName}s",
+            "name": galeryNaming.get(categoryType, f"Galerie - {categoryName}"),
+            "parent": galeryParent.get(categoryType, 856),# 856=En attente
             "method": "pwg.categories.add",
         }
         # Construire les données de la requête avec la pièce jointe
@@ -46,7 +60,8 @@ def createCategoryInPiwigo(categoryName="portrait"):
 if __name__=="__main__":
     seuilMin = 50
     seuilMax = 600
-    listcategoriespath = "D:/wamp64/www/givingsense.eu/datamusee/scrutart/src/generationWordpress/data/fr/listeGenresPeintures.json"
+    # listcategoriespath = "D:/wamp64/www/givingsense.eu/datamusee/scrutart/src/generationWordpress/data/fr/listeGenresPeintures.json"
+    listcategoriespath = "D:\wamp64\www\givingsense.eu\datamusee\scrutart\src\generationWordpress\data\wikidataSignificantPaintersTicket1527.json"
     listcat = []
     with open(listcategoriespath, "r", encoding="UTF-8") as fdata:
         data = json.loads(fdata.read())
@@ -57,7 +72,7 @@ if __name__=="__main__":
         idxsav = 0
         for cat in listcat:
             if (not "galerie" in cat) and (int(cat["c"])>=seuilMin)  and (int(cat["c"])<=seuilMax):
-                res = createCategoryInPiwigo(cat["genreLabel"])
+                res = createCategoryInPiwigo(cat["entityLabel"])
                 if res:
                     cat["galerie"] = res.text
                     idxsav += 1
