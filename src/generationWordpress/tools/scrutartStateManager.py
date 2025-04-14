06a5cb950caf536cbbf1b5c360d2b7cc23574626
moissina <jcmoissinac@gmail.com>
2025-04-14 17:31:10 +0200

    premier commit de ce projet de gestionnaire du graphe d'état de scrutart pour que toutes les consultations et mises à jour sur ce graphe

---
+++ src/generationWordpress/tools/scrutartStateManager.py
@@ -0,0 +1,20 @@
+import SPARQLWrapper as sw
+
+class scrutartStateManager():
+    def __init__(self, urlendpoint):
+        self.urlendpoint = urlendpoint
+        check = self.checkIfSparqlScrutartStateEndpointIsAvailable(urlendpoint)
+        pass
+
+    def checkIfSparqlScrutartStateEndpointIsAvailable(self, urlendpoint):
+        query = """select distinct ?s            where {              ?s ?p ?o            } LIMIT 1        """
+        sparqlScrutartWrapper = sw.SPARQLWrapper2(urlendpoint)  # implicit JSON format
+        sparqlScrutartWrapper.setQuery(query)
+        try:
+            res = sparqlScrutartWrapper.queryAndConvert()
+            return True if res and res.bindings else False
+        except Exception as e:
+            print(
+                    f"le serveur scrutart state (au 12/2/2025, {urlendpoint} (D:\Outils\Semantic/apache-jena-fuseki-4.8.0/fuseki-server) doit avoir été lancé avant de lancer cette application")
+            exit(7777)
+            return False
