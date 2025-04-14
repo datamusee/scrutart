cfb367274dd5d995d192031ef8c2513ac950122a
moissina <jcmoissinac@gmail.com>
2025-04-14 17:49:49 +0200

    premier commit d'exemple de code de création de catégories piwigo; voir des choses opérationnelles dans processMovementsCategories

---
+++ src/piwigoTools/buildListEmptyCategoriesCreateurs.py
@@ -0,0 +1,42 @@
+import json
+
+if __name__ == "__main__":
+    creatorsFilePath = "D:\wamp64\www\givingsense.eu\datamusee\scrutart\src\generationWordpress\data\wikidataSignificantPaintersTicket1527.json"
+    with open(creatorsFilePath, encoding="UTF-8") as creatorsSelectionFile:
+        creators = json.load(creatorsSelectionFile)
+        creatorsNameDict = {}
+        creatorsQidDict = {}
+        for crea in creators:
+            qid = crea["entity"].replace("http://www.wikidata.org/entity/","")
+            entityName = crea["entityLabel"]
+            type = "painter"
+            newelmt = {
+                "qid": qid,
+                "entityName": entityName,
+                "type": type
+            }
+            creatorsNameDict[entityName] = newelmt
+            creatorsQidDict[qid] = newelmt
+        pass
+    emptycats = []
+    with open("listCategories.2025.03.json", encoding="UTF-8") as catlistfile:
+        data = json.load(catlistfile)
+        catlist = data["result"]["categories"]
+        for cat in catlist:
+            if cat["nb_images"]==0:
+                emptycatdesc = {}
+                entityName = cat["name"].replace("Galerie ", "")
+                crea = creatorsNameDict.get(entityName, None)
+                if crea and crea["type"]=="painter":
+                    qid = crea["qid"]
+                    emptycatdesc["type"] = crea["type"]
+                    emptycatdesc["qid"] = qid
+                    emptycatdesc["categoryName"] = entityName
+                    compactName = entityName.replace(" ", "")
+                    emptycatdesc["piwigoCategory"] = cat["id"]
+                    emptycatdesc["listimagespath"] = \
+                        f"D:/wamp64/www/givingsense.eu/datamusee/scrutart/src/generationWordpress/data/fr/20250312/listeImages_{qid}_{compactName}.json"
+                    emptycats.append(emptycatdesc)
+            pass
+    with open("listeAlbumsEntities.json", "w", encoding="UTF-8") as albumsFile:
+        json.dump(emptycats, albumsFile, ensure_ascii=False)
