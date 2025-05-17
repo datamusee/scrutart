82311be7dcc2feec233e2b95bb0ee2e58e5b19df
moissina <jcmoissinac@gmail.com>
2025-05-17 14:32:09 +0200

    exploitation de listes json d'oeuvres avec image pour générer des ttl injectés dans scrutartState

--- src/generationWordpress/tools/imageArtisteListJsonToTTL.py
+++ src/generationWordpress/tools/imageArtisteListJsonToTTL.py
@@ -1,58 +1,35 @@
 
 import json
 import re
-import scrutartJsonToTtl
+from os import listdir
+from os.path import isfile, join
+from src.generationWordpress.CScrutartState import CScrutartState
 
-prefixList = [
-    "@prefix wdt: <http://www.wikidata.org/prop/direct/> .",
-    "@prefix wd: <http://www.wikidata.org/entity/> .",
-    "@prefix kgdc: <https://kg.grains-de-culture.fr/entity/> .",
-    "@prefix pgdc: <https://kg.grains-de-culture.fr/prop/> .",
-    "@prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .",
-    "@prefix xsd: <http://www.w3.org/2001/XMLSchema#> ."
-]
-
-labelsWikidataProperties = [
-    """wdt:P136 rdfs:label "genre"@fr .""",
-    """wdt:P31 rdfs:label "nature de l'élément"@fr .""",
-    """wdt:P170 rdfs:label "créateur"@fr .""",
-    """wdt:P18 rdfs:label "image"@fr ."""
-]
-
-
+# but: mettre des images dans le graphe d'état scrutartStateTrial (plus tard scrutartState local et TPT)
 if __name__ == "__main__":
-    converter = scrutartJsonToTtl.ScrutartJsonToTtl()
-    baseDir = "D:\wamp64\www\givingsense.eu\datamusee\scrutart\src\generationWordpress\data/fr/"
-    listFileToConvert = []
-    with open("listeJsonImagesArtiste.json", encoding="UTF-8") as flistsrc:
-        listFileToConvert = json.load(flistsrc)
+    baseDir = "D:\wamp64\www\givingsense.eu\datamusee\scrutart\src\generationWordpress\data/fr/fusion/"
+    listFileToConvert =[f for f in listdir(baseDir) if isfile(join(baseDir, f)) and ("listeImages_Q" in f) and (not ".ttl" in f)]
+    # fichier texte avec une simple liste de path vers des fichiers de liste d'images
     for fileName in listFileToConvert:
-        ttl = ""
-        ttl += "\n".join(prefixList) + "\n" + "\n"
-        ttl += "\n".join(labelsWikidataProperties) + "\n"
+        cs = CScrutartState()
         srcPath = baseDir + fileName
+        ttl = ""
         with open(srcPath, encoding="UTF-8") as fsrc:
             print(srcPath)
             src = json.load(fsrc)
-            imageDict = src["dict"]
-            for uriWork, imageDesc in imageDict.items():
-                if "http://www.wikidata.org/.well-known" in uriWork: continue
-                idImagePiwigo= idGaleryPiwigo = None
-                if ("posted" in imageDesc) and ("post_result" in imageDesc):
-                    regex = r'"image_id":(\d+).*?category\\\/(\d+)'
-                    match = re.search(regex, imageDesc["post_result"])
-                    if match:
-                        idImagePiwigo = match.group(1)
-                        idGaleryPiwigo = match.group(2)
-                qid = uriWork.replace("http://www.wikidata.org/entity/", "")
-                ttl += "\n"+converter.imageTtlDesc(uriWork, images=imageDesc["images"], piwigoImageId=idImagePiwigo,
-                                              piwigoCategoryId=idGaleryPiwigo,
-                                              createur=imageDesc["createur"],
-                                              createurLabel=imageDesc["createurLabel"],
-                                              titreImage=imageDesc["titre_fr"],
-                                              sparql=src["sparql"],
-                                              lang="fr")
-
-        ttlpath = srcPath.replace(".json", ".ttl")
-        with open(ttlpath, "w", encoding="UTF-8") as fttl:
-            fttl.write(ttl)
+            if "dict" in src:
+                imageDict = src["dict"]
+                for uriWork, imageDesc in imageDict.items():
+                    if "http://www.wikidata.org/.well-known" in uriWork: continue
+                    artwork = cs.artworkFromJson(imageDesc, src=src)
+                    ttl += artwork.toTtl()
+                    response = cs.putArtwork(artwork)
+            elif "liste" in src:
+                for imageDesc in src["liste"]:
+                    if "http://www.wikidata.org/.well-known" in imageDesc["uri"]: continue
+                    artwork = cs.artworkFromJson(imageDesc, src=src)
+                    ttl += artwork.toTtl()
+                    response = cs.putArtwork(artwork)
+            ttlpath = srcPath.replace(".json", ".ttl")
+            with open(ttlpath, "w", encoding="UTF-8") as fttl:
+                fttl.write(ttl)
