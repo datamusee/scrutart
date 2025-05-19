5839f6b774dabd965da2986989e5fb71defaec40
moissina <jcmoissinac@gmail.com>
2025-05-19 19:03:38 +0200

    envoi de description sur des catégories

--- src/piwigoTools/piwigoCategoriesComment.py
+++ src/piwigoTools/piwigoCategoriesComment.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
         for cat in listcat:
             if (int(cat["c"]) > seuil):
                 catid = json.loads(cat["galerie"])["result"]["id"]
-                res, comment = pwg.piwigo_comment_category(cat["entityLabel"], catid, cat["c"])
+                res, comment = pwg.piwigo_comment_category(cat["entityLabel"], catid)
                 if res:
                     cat["idpiwigo"] = catid
                     cat["comment"] = comment
